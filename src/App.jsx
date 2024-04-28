f2b8082da4faf4a6eb5ef42e245d4dc8f1cbfc1f
FELIPE ALVES <felipe2018cannavarro@gmail.com>
2024-04-28 19:02:38 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // https://script.google.com/macros/s/AKfycbyb28HuQQALVwFnLy7ZOGY_j288cf8yuvArKVeewescqr6vd4MzGDkU9k1QSn47fVAiYg/exec
+
+  const submit = (e) => {
+    const formElement = document.querySelector('form')
+    e.preventDefault()
+    console.log('Submitted')
+
+    const formData = new FormData(formElement)
+
+    fetch("https://script.google.com/macros/s/AKfycbyb28HuQQALVwFnLy7ZOGY_j288cf8yuvArKVeewescqr6vd4MzGDkU9k1QSn47fVAiYg/exec", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json()).then((data) => {
+      console.log(data)
+    }).catch((error) => console.log(error))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Contact Me Form</h1>
+      <h2>Start editing to see some magic happening</h2>
+      <form className='form' onSubmit={(e) => submit(e)}>
+        <input placeholder='Name' name='Name' type='text'></input>
+        <input placeholder='Phone' name='Phone' type='text'></input>
+        <input placeholder='Subject' name='Subject' type='text'></input>
+        <input placeholder='Email' name='Email' type='email'></input>
+        <input type="submit" className='button'/>
+      </form>
+
+    </div>
   )
 }
 
